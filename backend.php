19b1e4874ca9e9fc93d87c29c2f1dd22a4479d8c
chrisconte <chrisconte22@gmail.com>
2020-02-17 19:15:12 -0500

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,21 +1,18 @@
 <?php
-$_POST = json_decode(file_get_contents('php://input'), true);
 
-//Receive POST request with UCID and password
-if(isset($_POST['submit'])) {
-  $user_name = $_POST['ucid'];
-  $user_pass = $_POST['password'];
-}
+//Receive POST request data and assign to variables
+$user_name = $_POST['ucid'];
+$user_pass = $_POST['password'];
 
 //Connect to database
 $servername = "sql.njit.edu";
 $serveruser = "cc593";
-$serverpass = "XXXXXX";
+$serverpass = "XXXXXXXXX";
 $dbname = "cc593";
 $link = new mysqli($servername, $serveruser, $serverpass, $dbname);
 
-//Exit if connection to database cannot be established
-if($link->connect_errno > 0) {
+//Exit if connection to database cannot be formed
+if($link->connect_errno > 0){
   die('Unable to connect to database [' . $link->connect_error. ']');
 }
 
@@ -24,24 +21,23 @@ $query = "SELECT user, pass FROM account WHERE user = '$user_name'";
 $result = $link->query($query);
 
 //Exit if search query cannot be executed
-if(!$result = $link->query($query)) {
+if(!$result = $link->query($query)){
   die('There was an error running the query [' . $link->error. ']');
 }
 
 //Fetch query data
 $row = $result->fetch_assoc();
 
-//Validate credentials
-if($row['user']=="$user_name") {
-  if(password_verify($user_pass, $row['pass'])) {
+//Validate credentials and generate response
+if($row['user']=="$user_name"){
+  if(password_verify($user_pass, $row['pass'])){
     echo "Correct Credentials";
   }
-  else {
+  else{
     echo "Incorrect Password";
   }
 }
 else {
-  echo "Incorrect Username"; 
-  }
+  echo "Incorrect Username";
 }
 ?>
