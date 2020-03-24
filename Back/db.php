626c996659cc7d3ba95cc84cab8d4283a51de250
chrisconte <chrisconte22@gmail.com>
2020-03-23 20:37:08 -0400

    Establish connection to database

---
+++ Back/db.php
@@ -0,0 +1,18 @@
+<?php
+
+//Receive POST request data and assign to variables
+$user_name = $_POST['ucid'];
+$user_pass = $_POST['password'];
+
+//Connect to database
+$servername = "sql.njit.edu";
+$serveruser = "cc593";
+$serverpass = "CS490project!";
+$dbname = "cc593";
+$link = new mysqli($servername, $serveruser, $serverpass, $dbname);
+
+//Exit if connection to database cannot be formed
+if($link->connect_errno > 0){
+  die('Unable to connect to database [' . $link->connect_error. ']');
+}
+?>
